10f24d098d14b3edac70414f8104e78f32792b00
ThomasTekie00 <thtek2805@uit.no>
2025-03-23 20:34:42 +0100

    start

---
+++ config.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+#Setup:
+pygame.init()
+SCREEN_X = 1200
+SCREEN_Y = 800
+screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
+pygame.display.set_caption("Mayhem")
+running = True
+clock = pygame.time.Clock()
