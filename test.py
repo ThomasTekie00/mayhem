dd4ce4603a95696c84d8846ae53d748c01835a58
ThomasTekie00 <thtek2805@uit.no>
2025-03-24 16:57:00 +0100

    Bilder og en liten test forsøk

--- test.py
+++ test.py
@@ -1,24 +1,37 @@
 import pygame
 
 
-pygame.init
-
+pygame.init()
 run = True
-
-SCREEN_X = 1200 
-SCREEN_Y = int(SCREEN_X * 0.8)
-
+SCREEN_X = 1920
+SCREEN_Y = 1080
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
 pygame.display.set_caption("Mayhem")
 background_color = (0,0,0)
+test_font = pygame.font.Font("bilder/font.otf", 50)
+
+
+#Font
+text_surface = test_font.render("Mayheim", False, "red")
 
 
 #Filbane
+bk_img = "bilder/space.png"
+bakke_img = "bilder/bak1.png"
 p1_img = "p1.png"
 p2_img = "p2.png"
-rect_p1 = p1_img.get_rect()
-rect_p2 = p2_img.get_rect()
-rect.center = ()
+
+#Bakgrunnen
+background = pygame.image.load(bk_img)
+background = pygame.transform.scale(background,(SCREEN_X, SCREEN_Y))
+background = background.convert()
+#Får fargen til bunnen av skjermen slik at jeg kan bruke det som en platform
+background_color = background.get_at((SCREEN_X // 2, 1000))
+
+
+
+
 
 
 
@@ -26,6 +39,15 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+
+    screen.blit(background, (0,0))
+    pygame.draw.rect(screen, background_color , (0,1000, SCREEN_X, SCREEN_Y / 2))
+    screen.blit(text_surface, (800, 100))
+
+    pygame.display.flip()
+    clock.tick(60)
+    
 
 pygame.quit()
 
