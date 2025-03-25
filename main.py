9d5390218e7a77e6cac665a1cb4de2539e13a510
Gaute Heien <gaute.heien@outlook.com>
2025-03-25 14:21:10 +0100

    Litt main styr

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ class Player(pygame.sprite.Sprite):
     pass
 
 
+
+p1_img = "bilder/p1.png"
+p2_img = "bilder/p2.png"
+# Blir ikke samme class som player
+class Spaceship(pygame.sprite.Sprite):
+
+    def __init__(self):
+        image = pygame.image.load(p1_img).convert_alpha()
+        self.image = pygame.transform.scale(image, (42, 30))
+
+    def fire(sefl) :
+        pass
+
+    def update(self):
+        pass
